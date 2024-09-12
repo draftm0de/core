209aea0ff70ef20908c3b7cc8a1d00960d975238
Fux Peter <peter.fux@autopro24.at>
2024-09-12 23:24:08 +0200

    ..

--- .docker/caddy/script.sh
+++ .docker/caddy/script.sh
@@ -14,9 +14,14 @@ REPOSITORY="draftmode/base.caddy"
 # source snippets
 # -------------------------
 SCRIPT_PATH="/home/admin/projects/draftm0de/projects/base.scripts"
-source "${SCRIPT_PATH}/docker.build.sh"
-
+# source "${SCRIPT_PATH}/docker.build.sh"
 # clean "${REPOSITORY}"
-# build "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
+# build "${REPOSITORY}" ".build_args"
+# tag "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
 # push "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
-clean_remote_tags "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
+# clean "${REPOSITORY}"
+# clean_remote "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
+
+DELETE_REPOSITORY="${REPOSITORY}:1.0.0"
+RESPONSE=$("${SCRIPT_PATH}/docker.hub.sh" "delete" "${DELETE_REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}")
+echo ":$RESPONSE:"
