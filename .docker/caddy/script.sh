689e288ac815a527c57446663249fc2d69f5a628
Fux Peter <peter.fux@autopro24.at>
2024-10-03 00:11:53 +0200

    ..

--- .docker/caddy/script.sh
+++ .docker/caddy/script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-SECRET_PATH="../../.secrets"
 # -------------------------
 # with arguments
 # -------------------------
-CI_DOCKER_USERNAME=$(cat "$SECRET_PATH/DOCKER_USERNAME")
-CI_DOCKER_PASSWORD=$(cat "$SECRET_PATH/DOCKER_PASSWORD")
 REPOSITORY="draftmode/base.caddy"
 # -------------------------
 
@@ -14,14 +11,30 @@ REPOSITORY="draftmode/base.caddy"
 # source snippets
 # -------------------------
 SCRIPT_PATH="/home/admin/projects/draftm0de/projects/base.scripts"
-# source "${SCRIPT_PATH}/docker.build.sh"
-# clean "${REPOSITORY}"
-# build "${REPOSITORY}" ".build_args"
-# tag "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
+
+echo "[Notice] Build image..."
+BUILT_IMAGE_NAME="$REPOSITORY:latest"
+# ("${SCRIPT_PATH}/docker.sh" "build" "--build-args=.build_args" "-t $BUILT_IMAGE_NAME" ".")
+echo "[Notice] > build successfully"
+
+# get local SHA from built image
+echo "[Notice] Get local SHA for $BUILT_IMAGE_NAME"
+# SHA=$("${SCRIPT_PATH}/docker.sh" "image sha" "$BUILT_IMAGE_NAME")
+echo "[Notice] > local SHA for $BUILT_IMAGE_NAME: $SHA"
+
+# protect: for given IMAGE and SHA no remove tags exists
+# ("${SCRIPT_PATH}/docker.sh" "image tags" "$REPOSITORY" "--remote" "--sha=$SHA" "--exit")
+
+# get local SHA from built image
+echo "[Notice] Get latest remote tag for $BUILT_IMAGE_NAME "
+LATEST_TAG=$("${SCRIPT_PATH}/docker.sh" "image tags" "$BUILT_IMAGE_NAME" "--remote" "--latest=patch")
+echo "[Notice] > latest remote tag for $BUILT_IMAGE_NAME: $LATEST_TAG"
+
+#tag "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
 # push "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
 # clean "${REPOSITORY}"
 # clean_remote "${REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}"
 
-DELETE_REPOSITORY="${REPOSITORY}:1.0.0"
-RESPONSE=$("${SCRIPT_PATH}/docker.hub.sh" "delete" "${DELETE_REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}")
-echo ":$RESPONSE:"
+# DELETE_REPOSITORY="${REPOSITORY}:1.0.0"
+# RESPONSE=$("${SCRIPT_PATH}/docker.hub.sh" "delete" "${DELETE_REPOSITORY}" "${CI_DOCKER_USERNAME}" "${CI_DOCKER_PASSWORD}")
+# echo ":$RESPONSE:"
