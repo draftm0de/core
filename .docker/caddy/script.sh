8421b9d398ae98db0d563e6c4d3e12f98136d3b8
Fux Peter <peter.fux@autopro24.at>
2024-10-08 20:43:28 +0200

    ..

--- .docker/caddy/script.sh
+++ .docker/caddy/script.sh
@@ -18,23 +18,26 @@ BUILT_IMAGE_NAME="$REPOSITORY:latest"
 echo "[Notice] > build successfully"
 
 # get local SHA from built image
-echo "[Notice] Get local SHA for $BUILT_IMAGE_NAME"
-# SHA=$("${SCRIPT_PATH}/docker.sh" "image sha" "$BUILT_IMAGE_NAME")
-echo "[Notice] > local SHA for $BUILT_IMAGE_NAME: $SHA"
-
-# protect: for given IMAGE and SHA no remove tags exists
-echo "[Notice] Protect new SHA against remote"
-# ("${SCRIPT_PATH}/docker.sh" "image tags" "$REPOSITORY" "--remote" "--sha=$SHA" "--exit")
+echo "[Notice] Protect built SHA against remote"
+SHA=$("${SCRIPT_PATH}/docker.sh" "image sha" "$BUILT_IMAGE_NAME")
+("${SCRIPT_PATH}/docker.sh" "image tags" "$REPOSITORY" "--remote" "--sha=$SHA" "--exit")
 echo "[Notice] > Protection successfully"
 
 # get local SHA from built image
 echo "[Notice] Get latest remote tag for $BUILT_IMAGE_NAME "
-LATEST_TAG=$("${SCRIPT_PATH}/docker.sh" "image tags" "$BUILT_IMAGE_NAME" "--remote" "--latest=patch")
+# LATEST_TAG=$("${SCRIPT_PATH}/docker.sh" "image tags" "$BUILT_IMAGE_NAME" "--remote" "--latest=patch")
+LATEST_TAG="1.2.1"
 echo "[Notice] > latest remote tag for $BUILT_IMAGE_NAME: $LATEST_TAG"
 
-echo "[Notice] Get next image tags"
-NEXT_TAGS="1.2.3"
-echo "[Notice] > next image tags for $REPOSITORY: $NEXT_TAGS"
+echo "[Notice] Tag docker image"
+TAGS=$("${SCRIPT_PATH}/docker.sh" "image tag" "$BUILT_IMAGE_NAME" "$REPOSITORY:$LATEST_TAG" "--tag-increase" "--tag-level=3")
+echo "[Notice] > docker image tagged successfully"
 
-echo "[Notice] Tag and push images"
-echo "[Notice] > Tagging and pushing successfully"
+echo "[Notice] Push docker images"
+# shellcheck disable=SC2206
+TAGS=($TAGS)
+TAGS+=("$BUILT_IMAGE_NAME")
+for TAG in "${TAGS[@]}"; do
+  echo "docker push $TAG"
+done
+echo "[Notice] > Pushing docker images successfully"
